3b4ebc17611c456dcb903df045ead6d7db394eae
Samuel <samuelsoares177778@gmail.com>
2019-07-10 10:22:58 -0300

    adicionei rota login e tela de cadastro de usuario

--- jogoteca.py
+++ jogoteca.py
@@ -29,4 +29,14 @@ def criar():
     lista.append(jogo)
     return redirect('/')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    if 'mestra' == request['senha']:
+        return redirect('/')
+    else:
+        return redirect('/login')
 app.run(debug=True)
