d415cb7085de131c20e28f6af3d41011dfdc4934
Samuel <samuelsoares177778@gmail.com>
2019-07-17 11:40:39 -0300

    alterei as rotas para urls dinamicas

--- jogoteca.py
+++ jogoteca.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 app = Flask(__name__)
 app.secret_key = 'sessao'
+
 class Jogo:
     def __init__(self, nome, categoria, console):
         self.nome = nome
@@ -20,7 +21,7 @@ def index():
 @app.route('/novo')
 def novo():
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect('/login')
+        return redirect(url_for('login', proxima= url_for('novo'))) #chama a função login e passa a proxima pagina como parametro
     return render_template('novo.html', titulo='Novo Jogo')
 
 @app.route('/criar', methods=['POST'])
@@ -30,26 +31,28 @@ def criar():
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return redirect('/')
+    return redirect(url_for('index'))
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')# variavel que foi definida no redirect
+    return render_template('login.html', proxima=proxima)
 
 @app.route('/autenticar', methods=['POST'])
 def autenticar():
     if 'mestra' == request.form['senha']:
         session['usuario_logado'] = request.form['usuario'] #salva a sessão do usuario em um cookie
         flash(request.form['usuario'] + ' logou com sucesso!') #mostra uma msg rapida pro usuario
-        return redirect('/')
+        proxima_pagina = request.form['proxima']
+        return redirect(proxima_pagina)
     else:
         flash('Não logado, Tente Novamente')
-        return redirect('/login')
+        return redirect(url_for('login'))
 
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
     flash('Nenhum Usuario Logado!')
-    return redirect('/')
+    return redirect(url_for('index')) #o url_for é a função da rota que vai ser chamada
 
 app.run(debug=True)
