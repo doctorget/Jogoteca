29f3da37e3265d7a7d573561e66eac5af81fb0a9
Samuel <samuelsoares177778@gmail.com>
2019-08-19 20:34:56 -0300

    tabelas criadas

--- db.py
+++ db.py
@@ -3,12 +3,18 @@ import sqlite3
 database = sqlite3.connect('jogoteca.db')
 cursor = database.cursor()
 
-cursor.execute('''CREATE TABLE `jogo` (
+'''cursor.execute(CREATE TABLE `jogo` (
       id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL ,
       nome varchar(50) NOT NULL,
       categoria varchar(40) NOT NULL,
       console varchar(20) NOT NULL,
-    )''')
+    )'')'''
+
+'''cursor.execute(''CREATE TABLE usuario (
+      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      nome` varchar(20) NOT NULL,
+      senha` varchar(8) NOT NULL,
+    )'')'''
 
 
 '''cursor.executemany(
