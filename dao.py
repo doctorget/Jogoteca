2b13e8bc1a52457eb7e2f4c0826f92588d09a27f
getsam <samuelsoares177778@gmail.com>
2020-01-31 17:10:34 -0300

    ultima versão do projeto com flask e mysqldb

--- dao.py
+++ dao.py
@@ -1,51 +1,51 @@
-from jogoteca import Jogo, Usuario
+from models import Jogo, Usuario
 
-SQL_DELETA_JOGO = 'delete from jogo where id = ?'
-SQL_JOGO_POR_ID = 'SELECT id, nome, categoria, console from jogo where id = ?'
-SQL_USUARIO_POR_ID = 'SELECT id, nome, senha from usuario where id = ?'
-SQL_ATUALIZA_JOGO = 'UPDATE jogo SET nome=, categoria=?, console=? where id = ?'
+SQL_DELETA_JOGO = 'delete from jogo where id = %s'
+SQL_JOGO_POR_ID = 'SELECT id, nome, categoria, console from jogo where id = %s'
+SQL_USUARIO_POR_ID = 'SELECT id, nome, senha from usuario where id = %s'
+SQL_ATUALIZA_JOGO = 'UPDATE jogo SET nome=%s, categoria=%s, console=%s where id = %s'
 SQL_BUSCA_JOGOS = 'SELECT id, nome, categoria, console from jogo'
-SQL_CRIA_JOGO = 'INSERT into jogo (nome, categoria, console) values (?, ?, ?)'
+SQL_CRIA_JOGO = 'INSERT into jogo (nome, categoria, console) values (%s, %s, %s)'
 
 
 class JogoDao:
-    def __init__(self, database):
-        self.__database = database
+    def __init__(self, db):
+        self.__db = db
 
     def salvar(self, jogo):
-        cursor = self.__database.connection.cursor()
+        cursor = self.__db.connection.cursor()
 
         if (jogo.id):
-            cursor.execute(SQL_ATUALIZA_JOGO, (jogo.nome, jogo.categoria, jogo.console))
+            cursor.execute(SQL_ATUALIZA_JOGO, (jogo.nome, jogo.categoria, jogo.console, jogo.id))
         else:
             cursor.execute(SQL_CRIA_JOGO, (jogo.nome, jogo.categoria, jogo.console))
             jogo.id = cursor.lastrowid
-        self.__database.connection.commit()
+        self.__db.connection.commit()
         return jogo
 
     def listar(self):
-        cursor = self.__database.connection.cursor()
+        cursor = self.__db.connection.cursor()
         cursor.execute(SQL_BUSCA_JOGOS)
         jogos = traduz_jogos(cursor.fetchall())
         return jogos
 
     def busca_por_id(self, id):
-        cursor = self.__database.connection.cursor()
+        cursor = self.__db.connection.cursor()
         cursor.execute(SQL_JOGO_POR_ID, (id,))
         tupla = cursor.fetchone()
         return Jogo(tupla[1], tupla[2], tupla[3], id=tupla[0])
 
     def deletar(self, id):
-        self.__database.connection.cursor().execute(SQL_DELETA_JOGO, (id, ))
-        self.__database.connection.commit()
+        self.__db.connection.cursor().execute(SQL_DELETA_JOGO, (id, ))
+        self.__db.connection.commit()
 
 
 class UsuarioDao:
-    def __init__(self, database):
-        self.__database = database
+    def __init__(self, db):
+        self.__db = db
 
     def buscar_por_id(self, id):
-        cursor = self.__database.connection.cursor()
+        cursor = self.__db.connection.cursor()
         cursor.execute(SQL_USUARIO_POR_ID, (id,))
         dados = cursor.fetchone()
         usuario = traduz_usuario(dados) if dados else None
--- prepara_banco.py
+++ prepara_banco.py
@@ -1,43 +1,34 @@
-import sqlite3
-from wrap_connection import transact
-
-print('Conectando ...')
-
-conn = sqlite3.connect("jogoteca.db")
-
-def make_connection():
-    conn = sqlite3.connect("jogoteca.db")
-    return conn
+import MySQLdb
+print('Conectando...')
+conn = MySQLdb.connect(user='root', passwd='admin', host='localhost', port=3306)
 
 # Descomente se quiser desfazer o banco...
-#conn.cursor().execute("DROP DATABASE `jogoteca;")
+#conn.cursor().execute("DROP DATABASE `jogoteca`;")
 #conn.commit()
 
-criar_tabelas = '''
+criar_tabelas = '''SET NAMES utf8;
+    CREATE DATABASE `jogoteca` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+    USE `jogoteca`;
     CREATE TABLE `jogo` (
-      id int(11) NOT NULL PRIMARY KEY,
-      nome varchar(50) NOT NULL,
-      categoria varchar(40) NOT NULL,
-      console varchar(20) NOT NULL,
-    )
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `nome` varchar(50) COLLATE utf8_bin NOT NULL,
+      `categoria` varchar(40) COLLATE utf8_bin NOT NULL,
+      `console` varchar(20) NOT NULL,
+      PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
     CREATE TABLE `usuario` (
-      id varchar(8) NOT NULL,
-      nome` varchar(20) NOT NULL,
-      senha` varchar(8) NOT NULL,
-      PRIMARY KEY (id)
-    )'''
+      `id` varchar(8) COLLATE utf8_bin NOT NULL,
+      `nome` varchar(20) COLLATE utf8_bin NOT NULL,
+      `senha` varchar(8) COLLATE utf8_bin NOT NULL,
+      PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;'''
 
-@transact(make_connection)
-def criar_db():
-    conn.cursor().execute(criar_tabelas)
-    conn.commit()
-    
-criar_db()
+conn.cursor().execute(criar_tabelas)
 
 # inserindo usuarios
 cursor = conn.cursor()
 cursor.executemany(
-      'INSERT INTO jogoteca.usuario (id, nome, senha) VALUES (?, ?, ?)',
+      'INSERT INTO jogoteca.usuario (id, nome, senha) VALUES (%s, %s, %s)',
       [
             ('luan', 'Luan Marques', 'flask'),
             ('nico', 'Nico', '7a1'),
@@ -51,14 +42,14 @@ for user in cursor.fetchall():
 
 # inserindo jogos
 cursor.executemany(
-      'INSERT INTO jogoteca.jogo (nome, categoria, console) VALUES (?, ?, ?)',
+      'INSERT INTO jogoteca.jogo (nome, categoria, console) VALUES (%s, %s, %s)',
       [
-            ('God of War 4', 'Acao', 'PS4'),
+            ('God of War 4', 'Ação', 'PS4'),
             ('NBA 2k18', 'Esporte', 'Xbox One'),
             ('Rayman Legends', 'Indie', 'PS4'),
             ('Super Mario RPG', 'RPG', 'SNES'),
             ('Super Mario Kart', 'Corrida', 'SNES'),
-            ('Fire Emblem Echoes', 'Estrategia', '3DS'),
+            ('Fire Emblem Echoes', 'Estratégia', '3DS'),
       ])
 
 cursor.execute('select * from jogoteca.jogo')
